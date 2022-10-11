53e5b4de62795bb76550d5ee2ceff0c02d2222b7
MikkeBee <mjblakley87@gmail.com>
2022-10-11 19:44:35 +0300

    getting desired items from json, converted unixdate to human date

--- src/components/SingleStory/SingleStory.jsx
+++ src/components/SingleStory/SingleStory.jsx
@@ -3,15 +3,25 @@ import { useParams } from "react-router-dom";
 
 const SingleStory = ({ newsArticles }) => {
   const { id } = useParams();
-  console.log(id);
-  console.log(newsArticles);
-  const newsArticle = newsArticles.filter(
-    (article) => article.id === Number(id)
-  );
+  const newsArticle = newsArticles.find((article) => article.id === Number(id));
+  const unixConversion = () => {
+    const millisecs = newsArticle.time * 1000;
+    const dateObj = new Date(millisecs);
+    const readableDate = dateObj.toLocaleString();
+    return readableDate;
+  };
   console.log(newsArticle);
   return (
     <section>
       <h3>{newsArticle.title}</h3>
+      <p>Added by: {newsArticle.by}</p>
+      <p>Date: {unixConversion()}</p>
+      <p>
+        Link:{" "}
+        <a href="{newsArticle.url}" target="_blank">
+          {newsArticle.url}
+        </a>
+      </p>
     </section>
   );
 };
