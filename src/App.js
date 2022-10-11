30f1104442c2e2ec4a51a89824b69f89bfab00c6
MikkeBee <mjblakley87@gmail.com>
2022-10-11 19:08:34 +0300

    Move axios up to App level so I can pass data down

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import NewsGallery from "./components/NewsGallery/NewsGallery";
+import SingleStory from "./components/SingleStory/SingleStory";
 
 function App() {
+  const [newsArticles, setArticleData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then((res) => {
+        const ids = res.data.filter((id, index) => {
+          if (index < 20) {
+            return id;
+          }
+        });
+        console.log(ids);
+        axios
+          .all(
+            ids.map((id) =>
+              axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+            )
+          )
+          .then((data) => {
+            const articles = data.flat().map((item) => item.data);
+            setArticleData(articles);
+          });
+      });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<NewsGallery />} />
-          {/* <Route
-            path="/:id"
-            element={<SingleStory />}
-          /> */}
+          <Route index element={<NewsGallery newsArticles={newsArticles} />} />
+          <Route path="/:id" element={<SingleStory />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/NewsGallery/NewsGallery.jsx
+++ src/components/NewsGallery/NewsGallery.jsx
@@ -1,44 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import classes from "./newsgallery.module.css";
 
-const NewsGallery = () => {
-  const [testItem, setTestItem] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://hacker-news.firebaseio.com/v0/topstories.json")
-      .then((res) => {
-        const ids = res.data.filter((id, index) => {
-          if (index < 20) {
-            return id;
-          }
-        });
-        console.log(ids);
-        axios
-          .all(
-            ids.map((id) =>
-              axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-            )
-          )
-          .then((data) => {
-            const newsArticles = data.flat().map((item) => item.data);
-            setTestItem(newsArticles);
-          });
-      });
-  }, []);
-
-  console.log(testItem);
-
+const NewsGallery = ({ newsArticles }) => {
   return (
     <div className={classes.newsGallery}>
       <ol type="1">
-        {testItem
+        {newsArticles
           //   .filter((item, index) => index < 20)
-          .map((item) => (
-            <li key={item.id} className={classes.newsItem}>
-              <Link to={`/${item.id}`}>{item.title}</Link>
+          .map((newsArticle) => (
+            <li key={newsArticle.id} className={classes.newsItem}>
+              <Link to={`/${newsArticle.id}`}>{newsArticle.title}</Link>
             </li>
           ))}
       </ol>
