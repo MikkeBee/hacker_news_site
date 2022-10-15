fde154c08826511a58840e4403fcd651e5c9f71f
MikkeBee <mjblakley87@gmail.com>
2022-10-15 16:16:55 +0300

    Styled article links, added button component

---
+++ src/components/LoadButton/LoadButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./loadbutton.module.css";
+
+const LoadButton = () => {
+  return <button className={classes.loadButton}>Load more stories</button>;
+};
+
+export default LoadButton;
